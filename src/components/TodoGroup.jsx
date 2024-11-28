6b1b8e443787648ca194a5bf3e4ef91cb5aaadfb
VSHI <36368406@QQ.COM>
2024-11-28 23:08:24 +0800

    Feat: updated TodoGroup

---
+++ src/components/TodoGroup.jsx
@@ -0,0 +1,16 @@
+import {useContext} from "react";
+import {TodoListContext} from "../context/TodoListContext";
+import TodoItem from "./TodoItem";
+
+const TodoGroup = () => {
+  const {state} = useContext(TodoListContext)
+  return (
+      <div>
+        {state.map((todo) => {
+          return <TodoItem key={todo.id} todo={todo}/>
+        })}
+      </div>
+  );
+}
+
+export default TodoGroup;
