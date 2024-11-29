828e858a584130ae708ddd2a94ad37fff260a4a7
VSHI <36368406@QQ.COM>
2024-11-29 11:11:09 +0800

    feat: implement the router for the done list

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import TodoList from "./components/TodoList";
 import {initialState, todoReducer} from "./context/todoReducer";
 import {TodoListProvider} from "./context/TodoListContext";
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Link, Navigate, Route, Routes} from "react-router-dom"
 import PageNotFound from "./components/PageNotFound"
+import DoneList from "./components/DoneList";
 
 export const TodoContext = createContext();
 
@@ -15,9 +16,13 @@ function App() {
         <div className="App">
             <TodoListProvider>
                 <Router>
+                    <nav>
+                        <Link to={"/"}>Home</Link> | <Link to = "/donelist">Done List</Link>
+                    </nav>
                     <Routes>
                         <Route path={"/"} element={<Navigate to="/todo-list"/>}></Route>
                         <Route path={"/todo-list"} element={<TodoList/>}></Route>
+                        <Route path={"/donelist"} element = {<DoneList/>}></Route>
                         <Route path={"*"} element={<PageNotFound/>}></Route>
                     </Routes>
                 </Router>
