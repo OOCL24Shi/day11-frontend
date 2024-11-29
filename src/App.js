5a92b6baa528916fcc85de17a9387673038df1e6
VSHI <36368406@QQ.COM>
2024-11-30 00:12:27 +0800

    Feat: add help page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {TodoListProvider} from "./context/TodoListContext";
 import {BrowserRouter as Router, Link, Navigate, Route, Routes} from "react-router-dom"
 import PageNotFound from "./components/PageNotFound"
 import DoneList from "./components/DoneList";
+import Help from "./components/Help";
 
 export const TodoContext = createContext();
 
@@ -17,12 +18,13 @@ function App() {
             <TodoListProvider>
                 <Router>
                     <nav>
-                        <Link to={"/"}>Home</Link> | <Link to = "/donelist">Done List</Link>
+                        <Link to={"/"}>TodoList</Link> | <Link to = "/donelist">Done List</Link> | <Link to = "/help">Help</Link>
                     </nav>
                     <Routes>
                         <Route path={"/"} element={<Navigate to="/todo-list"/>}></Route>
                         <Route path={"/todo-list"} element={<TodoList/>}></Route>
                         <Route path={"/donelist"} element = {<DoneList/>}></Route>
+                        <Route path={"/help"} element={<Help/>}></Route>
                         <Route path={"*"} element={<PageNotFound/>}></Route>
                     </Routes>
                 </Router>
---
+++ src/components/Help.jsx
@@ -0,0 +1,11 @@
+const Help = () => {
+
+    return (
+        <div>
+            <h2>What can I help you with?</h2>
+        </div>
+
+    );
+}
+
+export default Help
