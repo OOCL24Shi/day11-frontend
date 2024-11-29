d83d8b70145dc12f3667805eb63388406499447e
VSHI <36368406@QQ.COM>
2024-11-29 10:42:46 +0800

    feat: implement the router for the todo list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { createContext, useReducer } from "react";
+import {createContext, useReducer} from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./context/todoReducer";
+import {initialState, todoReducer} from "./context/todoReducer";
 import {TodoListProvider} from "./context/TodoListContext";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom"
+import PageNotFound from "./components/PageNotFound"
 
 export const TodoContext = createContext();
 
 function App() {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+    const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  return (
-    <div className="App">
-      <TodoListProvider>
-        <TodoList/>
-      </TodoListProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TodoListProvider>
+                <Router>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to="/todo-list"/>}></Route>
+                        <Route path={"/todo-list"} element={<TodoList/>}></Route>
+                        <Route path={"*"} element={<PageNotFound/>}></Route>
+                    </Routes>
+                </Router>
+            </TodoListProvider>
+        </div>
+    );
 }
 
 export default App;
